ec9dc081bd15a9f75c402d19da3004c074e68528
SaidAsaad <saidyousef201513@gmail.com>
2025-07-07 12:26:22 -0300

    initial commit

---
+++ src/main/java/notificacao_de_agendamento/NotificacaoDeAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package notificacao_de_agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoDeAgendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoDeAgendamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/notificacao_de_agendamento/NotificacaoDeAgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package notificacao_de_agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoDeAgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
